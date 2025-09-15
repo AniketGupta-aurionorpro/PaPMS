3d5e29e29706fac297e73be1af74547497d13d7e
Aniket Gupta <aniket.gupta@aurionpro.com>
2025-09-16 00:46:08 +0530

    Uploaded Project Stucture

---
+++ src/main/java/com/aurionpro/papms/PaPmsApplication.java
@@ -0,0 +1,13 @@
+package com.aurionpro.papms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaPmsApplication.class, args);
+    }
+
+}
